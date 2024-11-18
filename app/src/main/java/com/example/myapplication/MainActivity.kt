8bea3f1ce03ce77d8b96b87dc26831b96a10c1cf
ethan <cgtslyTD@gmail.com>
2024-11-18 15:29:23 +0800

    test commit

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,36 +1,142 @@
 package com.example.myapplication
 
 import android.Manifest
+import android.annotation.SuppressLint
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothAdapter.STATE_CONNECTED
+import android.bluetooth.BluetoothDevice
+import android.bluetooth.BluetoothGatt
+import android.bluetooth.BluetoothGattCallback
+import android.bluetooth.BluetoothGattCharacteristic
+import android.bluetooth.BluetoothGattDescriptor
+import android.bluetooth.BluetoothManager
+import android.bluetooth.BluetoothProfile
+import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import android.os.Handler
+import android.os.HandlerThread
+import android.util.Log
 import android.view.View
+import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.ComponentActivity
+import androidx.activity.result.ActivityResultCallback
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.app.ActivityCompat
-import com.example.myapplication.bluetooth.BluetoothScanActivity
+import java.io.IOException
+import java.util.UUID
+import java.util.concurrent.CountDownLatch
+import java.util.concurrent.Executors
+import java.util.concurrent.TimeUnit
 
 class MainActivity : ComponentActivity() {
+    private val  TAG = "BluetoothDevice"
+    val executor = Executors.newSingleThreadExecutor()
+    lateinit var launcher: ActivityResultLauncher<Intent>
+    var currentSelectDevice: BluetoothDevice? = null
+    val deviceTv: TextView by lazy {
+        findViewById(R.id.device_tv)
+    }
+    val bondTv: TextView by lazy {
+        findViewById(R.id.bond_tv)
+    }
+
+    val connectTv: TextView by lazy {
+        findViewById(R.id.connected_tv)
+    }
+    val bluetoothAdapter by lazy {
+        (getSystemService(BLUETOOTH_SERVICE) as BluetoothManager).adapter
+    }
+
+    val handlerThread = HandlerThread("connect").apply {
+        start()
+    }
+    val handler = object : Handler(handlerThread.looper) {
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         title = "BYD Bluetooth connect"
+
+        launcher =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult(),
+                ActivityResultCallback {
+                    Log.e(
+                        "MainActivity",
+                        "onActivityResult: ${it.resultCode} ${
+                            it.data?.extras?.get("dataDevice").toString()
+                        }"
+                    )
+                    val result: BluetoothDevice? =
+                        it.data?.extras?.get("dataDevice") as? BluetoothDevice
+                    onDeviceSelected(result)
+
+                })
+    }
+
+    private fun loop() {
+        Log.e(TAG,"loop start")
+        currentSelectDevice?.run {
+            ConnectThread(
+                baseContext,
+                bluetoothAdapter!!,
+                address
+            ) {
+                handler.postDelayed({
+                    loop()
+                }, 2000)
+            }.run()
+        }
+
+
+    }
+
+    @SuppressLint("MissingPermission", "SetTextI18n")
+    private fun onDeviceSelected(result: BluetoothDevice?) {
+        currentSelectDevice = result
+        result?.run {
+            deviceTv.setText("${name} ${address}")
+            val bondState = bluetoothAdapter.getRemoteDevice(address).bondState
+            bondTv.text = "Bond status:" + when (bondState) {
+                BluetoothDevice.BOND_NONE -> {
+                    "Bond None"
+                }
+
+                BluetoothDevice.BOND_BONDING -> {
+                    "Bonding"
+                }
+
+                BluetoothDevice.BOND_BONDED -> {
+                    " Bonded"
+                }
+
+                else -> {
+                    "Unknown"
+                }
+            }
+        } ?: kotlin.run {
+            deviceTv.text = "No device selected"
+        }
     }
 
     fun scanBluetoothDevices(view: View) {
-        if (ActivityCompat.checkSelfPermission(
-                this,
-                Manifest.permission.BLUETOOTH_SCAN
-            ) != PackageManager.PERMISSION_GRANTED
-        ) {
+        val permissions: ArrayList<String> = arrayListOf(
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+            Manifest.permission.ACCESS_FINE_LOCATION,
+        )
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            permissions.add(Manifest.permission.BLUETOOTH_SCAN)
+            permissions.add(Manifest.permission.BLUETOOTH_CONNECT)
+        }
+        if (!checkAllPermissionGranted(permissions)) {
             ActivityCompat.requestPermissions(
                 this,
-                arrayOf(
-                    Manifest.permission.BLUETOOTH_SCAN,
-                    Manifest.permission.ACCESS_COARSE_LOCATION,
-                    Manifest.permission.ACCESS_FINE_LOCATION,
-                    Manifest.permission.BLUETOOTH_CONNECT
-                ),
+                permissions.toTypedArray(),
                 1
             )
             Toast.makeText(
@@ -38,9 +144,206 @@ class MainActivity : ComponentActivity() {
                 "Please grant permission to scan bluetooth devices",
                 Toast.LENGTH_SHORT
             ).show()
-            return
+
         } else {
-            startActivity(Intent(this, BluetoothScanActivity::class.java))
+            launcher.launch(
+                Intent(
+                    this,
+                    com.example.myapplication.bluetooth.BluetoothScanActivity::class.java
+                )
+            )
+        }
+    }
+
+    private fun checkAllPermissionGranted(permission: List<String>): Boolean {
+        permission.forEach {
+            if (ActivityCompat.checkSelfPermission(this, it) != PackageManager.PERMISSION_GRANTED
+            ) {
+                return false
+            }
+        }
+        return true
+    }
+
+    fun connectDevice(view: View) {
+        if (currentSelectDevice == null) {
+            Toast.makeText(this, "Please select a device to connect", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        ConnectThread(view.context, bluetoothAdapter!!, currentSelectDevice!!.address) {
+            handler.postDelayed({
+                loop()
+            }, 2000)
+        }.run()
+    }
+
+    @SuppressLint("MissingPermission")
+    private inner class ConnectThread(
+        val context: Context,
+        val bluetoothAdapter: BluetoothAdapter,
+        val address: String,
+        val endCallback: () -> Unit
+    ) {
+        private val device: BluetoothDevice by lazy {
+            bluetoothAdapter.getRemoteDevice(address)
+        }
+        private val socket: BluetoothGatt? by lazy(LazyThreadSafetyMode.NONE) {
+            device.connectGatt(context, false, object : BluetoothGattCallback() {
+                //连接状态变更
+                override fun onConnectionStateChange(
+                    gatt: BluetoothGatt?,
+                    status: Int,
+                    newState: Int
+                ) {
+                    Log.e("Connect", "onConnectionStateChange: $status $newState")
+                    if (newState == BluetoothProfile.STATE_CONNECTED) {
+                        runOnUiThread {
+                            bondTv.text = "Connected status: Connected"
+                        }
+                    } else if (newState == BluetoothProfile.STATE_DISCONNECTED) {
+                        runOnUiThread {
+                            bondTv.text = "Connected status: DisConnected"
+                        }
+                    }
+                }
+
+                //发现服务回调
+                override fun onServicesDiscovered(gatt: BluetoothGatt?, status: Int) {
+                    // 调用 mBleGatt?.discoverServices() 时触发该回调
+                    if (status != BluetoothGatt.GATT_SUCCESS) {
+                        //失败
+                        return
+                    }
+                    //获取指定GATT服务，UUID 由远程设备提供
+                    val bleGattService = socket?.getService(UUID.fromString("8888888"))
+                    //获取指定GATT特征，UUID 由远程设备提供
+                    val bleGattCharacteristic =
+                        bleGattService?.getCharacteristic(UUID.fromString("777777"))
+                    //启用特征通知，如果远程设备修改了特征，则会触发 onCharacteristicChange() 回调
+                    socket?.setCharacteristicNotification(bleGattCharacteristic, true)
+                    //启用客户端特征配置【固定写法】
+                    val bleGattDescriptor =
+                        bleGattCharacteristic?.getDescriptor(UUID.fromString("00002902-0000-1000-8000-00805f9b34fb"))
+                    bleGattDescriptor?.value = BluetoothGattDescriptor.ENABLE_INDICATION_VALUE
+                    socket?.writeDescriptor(bleGattDescriptor)
+                }
+
+                //启用客户端特征配置结果回调
+                override fun onDescriptorWrite(
+                    gatt: BluetoothGatt?,
+                    descriptor: BluetoothGattDescriptor?,
+                    status: Int
+                ) {
+                    if (status == BluetoothGatt.GATT_SUCCESS) {
+                        //此时蓝牙设备连接才算真正连接成功，即具备读写数据的能力
+                    }
+                }
+
+                //App修改特征回调，即 App 给设备发送数据结果回调
+                override fun onCharacteristicWrite(
+                    gatt: BluetoothGatt?, characteristic: BluetoothGattCharacteristic?, status: Int
+                ) {
+                    if (status == BluetoothGatt.GATT_SUCCESS) {
+                        //数据写入完成
+                        // 调用 characteristic?.value 得到的 ByteArray 与 发送数据一样
+                    }
+                }
+
+                //远程设备修改特征描述回调，即设备给 App 发送数据
+                override fun onCharacteristicChanged(
+                    gatt: BluetoothGatt?, characteristic: BluetoothGattCharacteristic?
+                ) {
+                    //调用 characteristic?.value 获取远程设备发送过来的数据
+                }
+            })
+        }
+
+        fun run() {
+            val hasConnected = isDeviceConnected();
+            Log.e(TAG, "HasConnected : $hasConnected")
+
+            if (hasConnected) {
+                endCallback.invoke()
+                return
+            }
+
+            socket?.let { socket ->
+                // Connect to the remote device through the socket. This call blocks
+                // until it succeeds or throws an exception.
+                while (true) {
+                    try {
+                        socket.connect()
+                        Log.e(TAG, "connect")
+                        break
+                    } catch (e: Exception) {
+                        Log.e(TAG, Log.getStackTraceString(e))
+                        Thread.sleep(1000)
+                    }
+                }
+                // The connection attempt succeeded. Perform work associated with
+                // the connection in a separate thread.
+            }
+            endCallback.invoke()
+        }
+
+        fun isDeviceConnected(): Boolean {
+
+            var isConnected = false
+            val countDownLatch = CountDownLatch(1)
+            executor.run {
+                bluetoothAdapter.getProfileProxy(
+                    context,
+                    object : BluetoothProfile.ServiceListener {
+                        /**
+                         * Called to notify the client when the proxy object has been
+                         * connected to the service.
+                         *
+                         * @param profile - One of [.HEADSET] or [.A2DP]
+                         * @param proxy - One of [BluetoothHeadset] or [BluetoothA2dp]
+                         */
+                        override fun onServiceConnected(profile: Int, proxy: BluetoothProfile?) {
+                            proxy?.getDevicesMatchingConnectionStates(intArrayOf(STATE_CONNECTED))
+                                ?.forEach { blueDevice ->
+                                    Log.e(
+                                        TAG,
+                                        "onServiceConnected "+blueDevice.name + "," + blueDevice.address
+                                    )
+                                    if (blueDevice.address.equals(device.address)) {
+                                        isConnected = true
+                                    }
+                                }
+                        }
+
+                        /**
+                         * Called to notify the client that this proxy object has been
+                         * disconnected from the service.
+                         *
+                         * @param profile - One of [.HEADSET] or [.A2DP]
+                         */
+                        override fun onServiceDisconnected(profile: Int) {
+                            Log.e(
+                                TAG,
+                                "onServiceDisconnected "
+                            )
+                        }
+
+                    },
+                    BluetoothProfile.GATT
+                )
+            }
+            val await = countDownLatch.await(5L, TimeUnit.SECONDS)
+            Log.e(TAG,"timeOut : $await" )
+            return isConnected
+        }
+
+        // Closes the client socket and causes the thread to finish.
+        fun cancel() {
+            try {
+                socket?.close()
+            } catch (e: IOException) {
+                Log.e("Connect", "Could not close the client socket", e)
+            }
         }
     }
 }
--- app/src/main/java/com/example/myapplication/bluetooth/BluetoothScanActivity.kt
+++ app/src/main/java/com/example/myapplication/bluetooth/BluetoothScanActivity.kt
@@ -3,36 +3,65 @@ package com.example.myapplication.bluetooth
 import android.annotation.SuppressLint
 import android.bluetooth.BluetoothDevice
 import android.bluetooth.BluetoothManager
+import android.bluetooth.le.ScanSettings
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
+import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
+import android.text.TextUtils
 import android.util.Log
+import android.view.View
+import android.view.View.OnClickListener
+import android.view.ViewGroup
+import android.view.ViewGroup.MarginLayoutParams
+import android.widget.Button
 import android.widget.TextView
 import androidx.activity.ComponentActivity
+import androidx.annotation.RequiresApi
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.myapplication.R
 
 class BluetoothScanActivity : ComponentActivity() {
 
+    val set: HashSet<String> = HashSet()
+    val arrayList: ArrayList<BluetoothDevice> = ArrayList()
+    val rv: RecyclerView by lazy {
+        findViewById(R.id.bluetooth_scan_recycler_view)
+    }
 
-    private val BLU
+
+
     // Create a BroadcastReceiver for ACTION_FOUND.
     private val receiver = object : BroadcastReceiver() {
 
+        @RequiresApi(Build.VERSION_CODES.R)
+        @SuppressLint("MissingPermission")
         override fun onReceive(context: Context, intent: Intent) {
             val action: String = intent.action ?: ""
             when (action) {
                 BluetoothDevice.ACTION_FOUND -> {
                     // Discovery has found a device. Get the BluetoothDevice
                     // object and its info from the Intent.
-                    val device: BluetoothDevice? =
-                        intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE)
-                    Log.d(
-                        "BluetoothScanActivity",
-                        "DeviceFound: ${device?.name} ${device?.address} ${device?.alias}"
-                    )
+
+                    intent.getParcelableExtra<BluetoothDevice?>(BluetoothDevice.EXTRA_DEVICE)?.run {
+                        val device = this
+                        Log.d(
+                            "BluetoothScanActivity",
+                            "DeviceFound: ${name} ${address} ${alias}"
+                        )
+                        if (!TextUtils.isEmpty(device.name) && !set.contains(device.address)) {
+                            set.add(device.address)
+                            arrayList.add(this)
+                            rv.adapter?.notifyItemInserted(arrayList.size - 1)
+
+                        }
+
+                    }
+
                 }
             }
         }
@@ -41,27 +70,70 @@ class BluetoothScanActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.bluetooth_scan_activity)
-        val rv =findViewById<RecyclerView>(R.id.bluetooth_scan_recycler_view)
-        rv.adapter = object : RecyclerView.Adapter<TextViewHolder>() {
-            override fun onCreateViewHolder(parent: android.view.ViewGroup, viewType: Int): TextViewHolderr {
-                return TextViewHolder(parent.context)
+        rv.layoutManager = LinearLayoutManager(this)
+        rv.adapter = object : RecyclerView.Adapter<TextViewHolder>(), OnClickListener {
+            override fun onCreateViewHolder(
+                parent: android.view.ViewGroup,
+                viewType: Int
+            ): TextViewHolder {
+                return TextViewHolder(parent.context, this)
             }
 
             override fun onBindViewHolder(holder: TextViewHolder, position: Int) {
+                holder.setData(arrayList[position])
             }
 
             override fun getItemCount(): Int {
-                return 0
+                return arrayList.size
+            }
+
+            /**
+             * Called when a view has been clicked.
+             *
+             * @param v The view that was clicked.
+             */
+            @SuppressLint("MissingPermission")
+            override fun onClick(v: View) {
+                val childLayoutPosition = rv.getChildLayoutPosition(v)
+                val data = arrayList.get(childLayoutPosition)
+
+                setResult(9, Intent().apply {
+                    putExtra("dataDevice", data)
+                })
+                finish()
             }
         }
         scan()
         pair()
     }
 
-    class TextViewHolder(context: Context) : RecyclerView.ViewHolder(TextView(context)){
+    class TextViewHolder(context: Context, onClickListener: OnClickListener) :
+        RecyclerView.ViewHolder(Button(context).apply {
+            layoutParams = MarginLayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT
+            ).apply {
+                setMargins(10, 10, 10, 10)
+            }
+            setBackgroundColor(R.color.purple_200)
+            setPadding(16, 16, 16, 16)
+            setTextColor(Color.parseColor("#ffffff"))
+            isAllCaps = false
+//        setSupportAllCaps(false)
+        }) {
         private val textView = itemView as TextView
 
+        init {
+            textView.setOnClickListener(onClickListener)
+        }
+
+        @SuppressLint("MissingPermission", "SetTextI18n")
+        fun setData(device: BluetoothDevice) {
+            textView.text =
+                "name: ${device.name} address: ${device.address}"
+        }
     }
+
 
     override fun onDestroy() {
         super.onDestroy()
@@ -75,6 +147,26 @@ class BluetoothScanActivity : ComponentActivity() {
 
 
         val bluetoothAdapter = (getSystemService(BLUETOOTH_SERVICE) as? BluetoothManager)?.adapter
+        bluetoothAdapter?.bluetoothLeScanner?.startScan(
+            null,
+            ScanSettings.Builder().setScanMode(ScanSettings.SCAN_MODE_BALANCED).build(),
+            object :
+                android.bluetooth.le.ScanCallback() {
+                override fun onScanResult(
+                    callbackType: Int,
+                    result: android.bluetooth.le.ScanResult?
+                ) {
+                    super.onScanResult(callbackType, result)
+                    if (result?.device?.name == null) {
+                        return
+                    }
+                    Log.d(
+                        "BluetoothScanActivity",
+                        "onScanResult: ${result?.device?.name} ${result?.device?.address} ${result?.rssi}"
+                    )
+                }
+            })
+
         val startDiscovery = bluetoothAdapter?.startDiscovery()
         Log.e("BluetoothScanActivity", "Scanning for devices $startDiscovery")
 
